d7ffe458c43088a15025ad54b407d3767590b8bd
jfs39 <jfs.serg@hotmail.com>
2020-02-19 12:37:24 -0500

    Version 0.0.2, data insertion into Database finally works

--- index.php
+++ index.php
@@ -12,16 +12,22 @@
     input[type=submit]{
         width:400px;
         height:50px;
-        
+        border: 2px solid black;
+        background-color: #e4f5e4;
+    }
+    input[type=submit]:hover{
+        border: 2px solid #6bbd68;
+        background-color: #a0e39d;
     }
     </style>
     <body>
     <form action="produit_envoyer.php" method="post">
-		<h1>Ajouter un produit v0.0.1</h1>
+		<h1>Ajouter un produit v0.0.2</h1>
         <br/>
         <p>
         <label for="nomProd">Nom du produit</label> : <input type="text" name="nomProd" id="nomProd" /><br />
         </p>
+        <br/>
         <p>
         <label for="texte">Description du produit</label> :  <textarea type="text" name="texteDesc" id="texteDesc" >Entrez la description du produit ici</textarea><br />
         </p>
@@ -29,12 +35,15 @@
         <p>
         <label for="prix">Prix du produit</label> :  <input type="number" name="prix" id="prix"/><br />
         </p>
+        <br/>
         <p>
 		<label for="texte">Autres détails</label> :  <textarea type="text" name="texteDet" id="texteDet" >Entrez les autres détails ici</textarea><br />
         </p>
+        <br/>
         <p>
         <label for="userid">Code d'utilisateur</label> :  <input type="number" name="userid" id="userid"/><br />
         </p>
+        <br/>
         <input type="submit" value="Envoyer le produit" />
 	
     </form>
@@ -42,24 +51,24 @@
     <?php
         try
         {
-            $bdd = new PDO('mysql:host=localhost;dbname=produitscaracteristiques;charset=utf8', 'root', 'maman28222');
+            $bdd = new PDO('mysql:host=localhost;dbname=produitscaracteristiques;charset=utf8', 'root', 'maman28222',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         }
         catch(Exception $e)
         {
             die('Erreur : '.$e->getMessage());
         }
 
-        $reponse = $bdd->query('SELECT * FROM products ORDER BY ID DESC LIMIT 0, 10');
+        $reponse = $bdd->query('SELECT * FROM products');
 
     while ($donnees = $reponse->fetch())
     {
-        echo '<p><a href="produit_modifier.php?id=' . $donnees['id'] . '">[modifier]</a> <strong>' . 
-            htmlspecialchars($donnees['auteur']) . 
+        echo '<p><a href="produit_modifier.php?id=' . $donnees['USER_ID'] . '">[modifier]</a> <strong>' . //Revoir les données et adapter pour faire beau
+            htmlspecialchars($donnees['PRODUCT_ID']) . 
             '</strong> (' . 
-            $donnees['date'] . ') : <em>' . 
-            htmlspecialchars($donnees['titre']) . '</em>, ' . 
-            htmlspecialchars($donnees['texte']) . 
-            ' (privé : ' . htmlspecialchars($donnees['prive']) . 
+            $donnees['Product_Name'] . ') : <em>' . 
+            htmlspecialchars($donnees['Product_Description']) . '</em>, ' . 
+            htmlspecialchars($donnees['Price']) . 
+            ' (privé : ' . htmlspecialchars($donnees['Other_Details']) . 
             ')</p>';
     }
 
--- produit_envoyer.php
+++ produit_envoyer.php
@@ -1,12 +1,12 @@
 <?php
 // Connexion à la base de données
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=produitscaracteristiques;charset=utf8', 'root', 'maman28222');
+    $bdd = new PDO('mysql:host=localhost;dbname=produitscaracteristiques;charset=utf8', 'root', 'maman28222' ,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
 // Insertion du commentaire à l'aide d'une requête préparée
-$req = $bdd->prepare('INSERT INTO products(PRODUCT_ID,Product_Name, Product_Description, Price, Other_Details,USER_ID) VALUES( null,?, ?, ?, ?,?);'); 
+$req = $bdd->prepare('INSERT INTO products(Product_Name, Product_Description, Price, Other_Details,USER_ID) VALUES( ?, ?, ?, ?,?);'); 
 $req->execute(array($_POST['nomProd'], $_POST['texteDesc'], $_POST['prix'], $_POST['texteDet'], $_POST['userid']));
 
 //header('Location: index.php');
